fa1cac090884a75a42945680d935d11f6cf73ebd
Eliottln <eliott.le-normand@etu.univ-lyon1.fr>
2023-04-05 09:42:16 +0200

    fix bottom navbar

--- app/src/main/java/com/example/rickandmortyunivers/ui/MainActivity.kt
+++ app/src/main/java/com/example/rickandmortyunivers/ui/MainActivity.kt
@@ -2,28 +2,24 @@ package com.example.rickandmortyunivers.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.rickandmortyunivers.R
 import com.example.rickandmortyunivers.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val navController: NavController
+        get() = (supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment).navController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-//        if (savedInstanceState == null) {
-//            supportFragmentManager.beginTransaction()
-//                .replace(R.id.container, CharactersListFragment.newInstance())
-//                .commitNow()
-//        }
-        val bottomNav = findViewById<BottomNavigationView>(R.id.main_navigation)
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment
-        val navController = navHostFragment.navController
-        bottomNav?.setupWithNavController(navController)
+
+        binding.bottomNav.setupWithNavController(navController)
     }
 
 }
